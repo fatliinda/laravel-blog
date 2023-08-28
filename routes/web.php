c29e2f8e28bd5d93ca1b8dfd1cdd7fc809373c67
tech387-development <136323276+tech387-development@users.noreply.github.com>
2023-08-28 22:05:47 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PagesController::class,'index']);
+Route::resource('/blog',PostsController::class);
+require __DIR__.'/auth.php';
+
+require __DIR__.'/auth.php';
