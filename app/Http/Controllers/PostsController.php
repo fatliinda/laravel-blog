25122c5f66ec6a0bd13bd296acca01995cd9b09c
tech387-development <136323276+tech387-development@users.noreply.github.com>
2023-08-30 00:36:24 +0200

    showing posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
@@ -11,7 +12,10 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $post=Post::all();
+    
+        return view('layouts.blog')->with('posts',Post::orderBy('updated_at','DESC')->get());
+
     }
 
     /**
---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.layout')
+
+
+
+@section('styles')
+    <!-- Additional styles for this page -->
+    <style>
+        /* Your page-specific styles */
+    </style>
+@endsection
+
+@section('content')
+@foreach($posts as $post)
+<section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+            <article class="flex flex-col shadow my-4">
+                <!-- Article Image -->
+                <a href="#" class="hover:opacity-75">
+                    <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
+                </a>
+                <div class="bg-white flex flex-col justify-start p-6">
+                    <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
+                    <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{$post->title}}</a>
+                    <p href="#" class="text-sm pb-3">
+                        By <a href="#" class="font-semibold hover:text-gray-800">{{$post->user->name}}</a>, Written at{{date('jS M Y',strtotime($post->updated_at))}}
+                    </p>
+                    <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
+                    <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </article>
+@endforeach
+@endsection
